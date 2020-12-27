3a3ad158a4e142b594eb6ab192600aac97d8b566
pushparajkvp <pushparajkvp@gmail.com>
2020-12-27 16:45:12 +0530

    added adding numbers in ll format

---
+++ src/AddLLNumbers.java
@@ -0,0 +1,69 @@
+/**
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+
+
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode trav = head;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+            carry = sum / 10;
+            
+            ListNode newNode = new ListNode();
+            newNode.val = sum % 10;
+            
+            trav.next = newNode;
+            trav = trav.next;
+            
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+        
+        if(carry != 0) {
+            ListNode newNode = new ListNode();
+            newNode.val = carry;
+            
+            trav.next = newNode;
+            trav = trav.next;
+        }
+        return head.next;
+    }
+}
