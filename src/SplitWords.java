039fdf941d522764a58e3911f530e0f89bcfd108
pushparajkvp <pushparajkvp@gmail.com>
2021-01-13 23:07:32 +0530

    added dp solution

--- src/SplitWords.java
+++ src/SplitWords.java
@@ -49,3 +49,23 @@ class Solution {
         return mem[start] = false;
     }
 }
+
+//DP solution
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        
+        for(int i=0; i<s.length(); i++) {
+            for(int j=0; j<=i; j++) {
+                if(dp[j] && set.contains(s.substring(j, i+1))) {
+                    dp[i+1] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[s.length()];
+    }
+}
