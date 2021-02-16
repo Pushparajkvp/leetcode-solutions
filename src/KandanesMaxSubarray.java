42abd1c5ad8f682fbcb3680b9c1ce8183fc028b2
pushparajkvp <pushparajkvp@gmail.com>
2021-02-16 18:18:03 +0530

    added kandane's algo

---
+++ src/KandanesMaxSubarray.java
@@ -0,0 +1,47 @@
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [0]
+Output: 0
+Example 4:
+
+Input: nums = [-1]
+Output: -1
+Example 5:
+
+Input: nums = [-100000]
+Output: -100000
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-105 <= nums[i] <= 105
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length, max = nums[0];
+        
+        for(int it=1; it<n; it++) {
+            if(nums[it-1] > 0) nums[it] += nums[it-1];
+            max = Math.max(max, nums[it]);
+        }
+        return max;
+    }
+}
