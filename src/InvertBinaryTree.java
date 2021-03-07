f72c512c632037af7e100edf364a7ff52718df2f
pushparajkvp <pushparajkvp@gmail.com>
2021-03-07 21:37:57 +0530

    added solution for inverting a binary tree

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,51 @@
+/**
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        return invert(root);
+    }
+    
+    private TreeNode invert(TreeNode node) {
+        if(node == null)
+            return null;
+        
+        TreeNode right = node.right;
+        
+        node.right = invert(node.left);
+        node.left = invert(right);
+        
+        return node;
+    }
+}
