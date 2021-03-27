2fead233d180bc118e361574869c6ea068a4a939
pushparajkvp <pushparajkvp@gmail.com>
2021-03-27 22:27:30 +0530

    added robot cleaner problem

---
+++ src/RobotCleaner.java
@@ -0,0 +1,37 @@
+class Solution {
+  // going clockwise : 0: 'up', 1: 'right', 2: 'down', 3: 'left'
+  int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+  Set<Pair<Integer, Integer>> visited = new HashSet();
+  Robot robot;
+
+  public void goBack() {
+    robot.turnRight();
+    robot.turnRight();
+    robot.move();
+    robot.turnRight();
+    robot.turnRight();
+  }
+
+  public void backtrack(int row, int col, int d) {
+    visited.add(new Pair(row, col));
+    robot.clean();
+    // going clockwise : 0: 'up', 1: 'right', 2: 'down', 3: 'left'
+    for (int i = 0; i < 4; ++i) {
+      int newD = (d + i) % 4;
+      int newRow = row + directions[newD][0];
+      int newCol = col + directions[newD][1];
+
+      if (!visited.contains(new Pair(newRow, newCol)) && robot.move()) {
+        backtrack(newRow, newCol, newD);
+        goBack();
+      }
+      // turn the robot following chosen direction : clockwise
+      robot.turnRight();
+    }
+  }
+
+  public void cleanRoom(Robot robot) {
+    this.robot = robot;
+    backtrack(0, 0, 0);
+  }
+}
