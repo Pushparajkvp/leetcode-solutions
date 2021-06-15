3a527138fbad178b584b5493d147e3849aec4a74
pushparajkvp <pushparajkvp@gmail.com>
2021-06-15 23:36:22 +0530

    added solution for move zeros problem

---
+++ src/MoveZeros.java
@@ -0,0 +1,43 @@
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+*/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZeroIndex = 0;
+        
+        for(int it=0; it<nums.length; it++) {
+            if(nums[it] != 0)
+                swap(nums, nonZeroIndex++, it);
+        }
+        
+    }
+    
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+}
